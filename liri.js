5e9c4551669f453276a704ec18a18198fb566635
George Ferguson <Bhearre@users.noreply.github.com>
2026-04-10 10:36:19 -0400

    Update liri.js with new Spotify API and dependencies
    Refactor liri.js to use updated Spotify API and libraries.

--- liri.js
+++ liri.js
@@ -1,18 +1,203 @@
 require("dotenv").config();
-var Spotify = require('node-spotify-api');
-var axios = require('axios');
-var Table = require('cli-table');
-var keys = require("./keys.js");
-var fs = require('fs');
-var moment = require("moment");
-var execSync = require("child_process").execSync;
+const SpotifyWebApi = require('spotify-web-api-node');
+const axios = require('axios');
+const Table = require('cli-table3');
+const keys = require("./keys.js");
+const fs = require('fs');
+const { format } = require('date-fns');
+const execSync = require("child_process").execSync;
 
-var commandsMap = {
+const commandsMap = {
     // command  -------> What to do
     'spotify-this-song': searchSpotify,
     'concert-this': searchBandsInTown,
     'movie-this': searchIMDB,
     'do-what-it-says': doRandom
+}
+
+const command = (process.argv[2] || '').toLowerCase()
+const query = process.argv[3]
+
+if (!commandsMap[command]) {
+    console.error('Sorry, ' + command + ' is not a valid command')
+    process.exit(1)
+}
+
+commandsMap[command](query)
+
+
+
+async function searchSpotify(songName) {
+    const spotify = new SpotifyWebApi(keys.spotify);
+    
+    try {
+        const data = await spotify.searchTracks(songName);
+        const items = data.body.tracks.items
+
+        if (items.length === 0) {
+            return searchSpotify('The Sign, Ace of Base');
+        }
+
+        const table = new Table({
+            head: ['Artist Name', 'Song Name', 'Album', 'Preview URL']
+        });
+
+        for (let i = 0; i < items.length; i++) {
+            const item = items[i] // Current Item we're working with
+            let previewURL = item.preview_url
+            if (!previewURL) previewURL = 'N/A'
+           
+            const artistNames = []
+            for (let j = 0; j < item.artists.length; j++) {
+                artistNames.push(item.artists[j].name);
+            }
+            table.push(
+                [
+                    artistNames.join(', '),
+                    item.name,
+                    item.album.name,
+                    previewURL
+                ]
+            );
+        }
+        console.log(table.toString());
+        fs.writeFileSync('./test.json', JSON.stringify(data, null, 2), 'utf8')
+    } catch (error) {
+        console.log('Error occurred: ' + error);
+    }
+}
+
+function searchBandsInTown(bandName) {
+    const url = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp";
+    axios.get(url)
+        .then(function (response) {
+            const table = new Table({
+                head: ['Name of Venue', 'Venue Location', 'Date of Event']
+            });
+
+            for (let i = 0; i < response.data.length; i++) {
+                const event = response.data[i]
+                let location = event.venue.city
+                if (event.venue.region) location = location + " " + event.venue.region
+                if (event.venue.country) location = location + ", " + event.venue.country
+                // Using date-fns instead of moment
+                const startTime = format(new Date(event.datetime), 'MM/dd/yyyy');
+                table.push(
+                    [
+                        event.venue.name,
+                        location,
+                        startTime
+                    ]
+                );
+            }
+            console.log(table.toString());
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+
+
+
+function searchIMDB(movieName) {
+
+
+    const titleTable = new Table({
+        head: ['Title', 'Year', 'IMDB Rating', "Rotten Tomatoes Rating", 'Country', 'Language', 'Plot', 'Actors']
+    });
+// ===============================  Code Below Will Use the "Search" Endpoint of the OMDB API and return 10 results. ===========
+    // const url = "http://www.omdbapi.com/?s=" + movieName + "&apikey=e4179ba0&page=1&type=movie&Content-Type=application/json";
+    // axios.get(url)
+    //     .then(function (response) {
+    //         const searchMe = response.data.Search;
+    //         //console.log("length of search " + searchMe.length);
+
+    //         for (let i = 0; i < searchMe.length; i++) {
+    //             searchTable.push(
+    //                 [
+    //                     searchMe[i].Title,
+    //                     searchMe[i].Year                   
+    //                 ]
+    //             )
+    //         } //closes for loop
+
+    //         console.log(searchTable.toString());
+    //         //fs.writeFileSync('./searchIMDB.json', JSON.stringify(response.data, null, 2), 'utf8');
+
+    //     //search individual titles
+
+    //    // console.log("this is searchTable " + searchTable);
+
+    //     for (let k = 0; k < searchMe.length; k++) {
+
+    //  const searchTitle = searchMe[k].Title;
+    //const searchTitle = searchTable[k].Title;
+    //console.log("this is searchTitle " + searchTitle);
+// ========================================End code to Search for Multiple titles ======================================
+
+    const url = "http://www.omdbapi.com/?t=" + movieName + "&apikey=e4179ba0";
+    axios.get(url)
+        .then(function (response) {
+            const thisTitle = response.data;
+            //console.log("this is in the title search " + thisTitle.Title + " " + thisTitle.imdbRating);
+            titleTable.push([
+                thisTitle.Title,
+                thisTitle.Year,
+                thisTitle.imdbRating,
+                thisTitle.Ratings[1].Value,
+                thisTitle.Country,
+                thisTitle.Language,
+                thisTitle.Plot,
+                thisTitle.Actors
+            ])
+
+            console.log(titleTable.toString());    //this works but provides a cumulative table
+
+        }) //closes then function in search each title
+
+
+}
+
+
+
+// Liri Bot fourth API Call Function to read the file random.txt and do what it says.  Presently the function works to return the 
+// parameters of the random.txt file to the terminal but the functionality for running at the command line automagically is not yet in place.
+function doRandom() {
+
+    console.log('gonna do the random thing');
+    let command;
+    fs.readFile('./random.txt', 'utf8', function (err, contents) {
+        if (err) {
+            console.error('Error reading random.txt:', err);
+            return;
+        }
+        console.log(contents);
+        const args = contents.split(",");
+        command = args[0].trim();
+        const query = args[1].trim();
+
+        switch (command) {
+                case 'spotify-this-song':
+                    searchSpotify(query)
+                    break;
+                case 'concert-this':
+                    searchBandsInTown(query)
+                    break;
+                case 'movie-this':
+                    searchIMDB(query)
+                    break;
+               
+            }
+            
+    });
+   
+
+    console.log('after calling readFile');
+
+
+}
+   
 }
 
 var command = (process.argv[2] || '').toLowerCase()
